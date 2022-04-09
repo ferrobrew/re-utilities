d03cdc6b0745a49126e72d7da0feb7495d6f8cfa
Philpax <me@philpax.me>
2022-04-09 05:22:19 +0200

    util: add singleton macro

--- src/util.rs
+++ src/util.rs
@@ -5,3 +5,34 @@ pub unsafe fn make_ptr<U>(ptr: usize) -> *mut U {
 pub unsafe fn make_ptr_with_offset<U>(ptr: usize, offset: isize) -> *mut U {
     (ptr as *mut u8).offset(offset) as *mut U
 }
+
+// this should probably be a derive macro
+#[macro_export]
+macro_rules! singleton {
+    ($class_name:ty $(, $arg_name:ident : $arg_type:ty)*) => {
+        static mut INSTANCE: Option<$class_name> = None;
+
+        impl $class_name {
+            pub fn create($($arg_name : $arg_type),*) -> anyhow::Result<()> {
+                unsafe {
+                    INSTANCE = Some(<$class_name>::new( $($arg_name),* )?);
+                }
+                Ok(())
+            }
+
+            pub fn destroy() {
+                unsafe { std::mem::drop(INSTANCE.take()); }
+            }
+
+            #[allow(dead_code)]
+            pub fn get() -> Option<&'static $class_name> {
+                unsafe { INSTANCE.as_ref() }
+            }
+
+            #[allow(dead_code)]
+            pub fn get_mut() -> Option<&'static mut $class_name> {
+                unsafe { INSTANCE.as_mut() }
+            }
+        }
+    };
+}
